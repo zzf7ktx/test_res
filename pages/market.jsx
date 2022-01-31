4c40d02b0ad0049f84c0df1a3eddadde595eaab9
zzf7ktx <zzf7ktx@gmail.com>
2022-01-31 18:14:24 +0700

    Add involve component, add support in-app purchase for related NFTs functions

--- pages/market.jsx
+++ pages/market.jsx
@@ -1,27 +1,42 @@
 import Layout from 'components/Layout';
-import ItemCard from '../../components/itemcard/ItemCard';
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import {
-  marketActions,
-  marketSelector,
-} from '../../features/market/marketSlice';
+import ItemCard from 'components/itemcard/ItemCard';
+import { useAppSelector, useAppDispatch } from 'app/hooks';
+import { marketActions, marketSelector } from 'features/market/marketSlice';
 import { useEffect, useState } from 'react';
 import { Modal, Spinner } from 'react-bootstrap';
-import ItemDetail from '../../components/itemdetail/ItemDetail';
-import FloatingButton from '../../components/floatingbutton/FloatingButton';
-import PageTitle from '../../components/pagetitle/PageTitle';
+import ItemDetail from 'components/itemdetail/ItemDetail';
+import FloatingButton from 'components/floatingbutton/FloatingButton';
+import PageTitle from 'components/pagetitle/PageTitle';
 import { FiPlus, FiBriefcase } from 'react-icons/fi';
 import Web3Modal from 'web3modal';
 import { ethers } from 'ethers';
-import { nftaddress, nftmarketaddress } from '../../config';
-import NFT from '../../artifacts/contracts/NFT.sol/NFT.json';
-import NFTMarket from '../../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
+import { nftaddress, nftmarketaddress } from '../config';
+import NFT from 'artifacts/contracts/NFT.sol/NFT.json';
+import NFTMarket from 'artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 import { useRouter } from 'next/router';
+import { localWeb3, magicLocal } from 'app/magic';
 
 const Market = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const arrayItems = useAppSelector(marketSelector);
+
+  const [magic, setMagic] = useState(magicLocal);
+  const web3 = localWeb3();
+  const [userMetadata, setUserMetadata] = useState();
+  const [balance, setBalance] = useState('...');
+
+  useEffect(() => {
+    magic.user.isLoggedIn().then((magicIsLoggedIn) => {
+      if (magicIsLoggedIn) {
+        magic.user.getMetadata().then((user) => {
+          setUserMetadata(user);
+        });
+      } else {
+        console.log('retrieve to login');
+      }
+    });
+  }, [magicLocal]);
 
   useEffect(() => {
     dispatch(marketActions.fetchItems());
@@ -66,13 +81,49 @@ const Market = () => {
     }
   };
 
+  const fetchBalance = (address) => {
+    web3.eth
+      .getBalance(address)
+      .then((bal) => setBalance(web3.utils.fromWei(bal)));
+  };
+
+  const buyNftLocal = async (nft) => {
+    try {
+      const from = userMetadata.publicAddress;
+      const contract = new web3.eth.Contract(NFTMarket.abi, nftmarketaddress);
+      const price = ethers.utils.parseUnits(nft.price.toString(), 'ether');
+
+      // Estimate gas and get balance
+      const gas = await contract.methods
+        .createMarketSale(nftaddress, nft.marketId)
+        .estimateGas({
+          from,
+          value: price,
+        });
+      // Show caller balance and estimate fee
+      // Send transaction
+      const transaction = await contract.methods
+        .createMarketSale(nftaddress, nft.marketId)
+        .send({
+          from,
+          value: price,
+          gas,
+        });
+      // Check result and show them for caller
+      // Re-direct to assets page
+      router.push('/assets');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='row w-100'>
-      <div className='col-xl-12 pe-0'>
-        <FloatingButton icon={<FiPlus />} href={`/create`} />
+      <div className='col-xl-12 pe-0 middle-wrap'>
+        <FloatingButton icon={<FiPlus />} href={`/post/create`} />
         <FloatingButton icon={<FiBriefcase />} href={`/assets`} index={1} />
         <PageTitle title={'Market'} />
-        <div className='row m-0 p-0'>
+        <div className='row ms-0 w-100'>
           {arrayItems.isLoading ? (
             <Spinner animation='border' role='status'>
               <span className='visually-hidden'>Loading...</span>
@@ -81,7 +132,7 @@ const Market = () => {
             <h3>No item</h3>
           ) : (
             arrayItems.data.map((item, index) => (
-              <div className='col-4 mt-0 pt-0' key={index}>
+              <div className='col-sm-4 p-0 me-3 mb-3' key={index}>
                 <ItemCard item={item} onClick={handleShow(index)} />
                 <Modal
                   contentClassName='rounded-xxl'
@@ -89,7 +140,7 @@ const Market = () => {
                   show={show.includes(index)}
                   onHide={handleClose(index)}
                 >
-                  <ItemDetail item={item} onAction={buyNft} />
+                  <ItemDetail item={item} onAction={buyNftLocal} />
                 </Modal>
               </div>
             ))
